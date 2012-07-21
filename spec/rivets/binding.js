9b3d6099a9767c55bc5486f46a37c1c64d71142e
Michael Richards <mike22e@gmail.com>
2012-07-20 22:49:35 -0700

    Add initial set of tests for Rivets.Binding.

---
+++ spec/rivets/binding.js
@@ -0,0 +1,66 @@
+describe('Rivets.Binding', function() {
+  var model, el, view, binding;
+
+  beforeEach(function() {
+    rivets.configure({
+      adapter: {
+        subscribe: function() {},
+        unsubscribe: function() {},
+        read: function() {},
+        publish: function() {}
+      }
+    });
+
+    el = document.createElement('div');
+    el.setAttribute('data-text', 'obj.name');
+    view = rivets.bind(el, {obj: {}});
+    binding = view.bindings[0];
+  });
+
+  it('gets assigned the routine function matching the identifier', function() {
+    expect(binding.routine).toBe(rivets.routines.text);
+  });
+
+  describe('set()', function() {
+    it('performs the binding routine with the supplied value', function() {
+      spyOn(binding, 'routine');
+      binding.set('sweater');
+      expect(binding.routine).toHaveBeenCalledWith(el, 'sweater');
+    });
+
+    it('applies any formatters to the value before performing the routine', function() {
+      rivets.config.formatters = {
+        awesome: function(value) { return 'awesome ' + value }
+      };
+      binding.formatters.push('awesome');
+      spyOn(binding, 'routine');
+      binding.set('sweater');
+      expect(binding.routine).toHaveBeenCalledWith(el, 'awesome sweater');
+    });
+
+    describe('on an event binding', function() {
+      beforeEach(function() {
+        binding.bindType = 'event';
+      });
+
+      it('performs the binding routine with the supplied function and current listener', function() {
+        spyOn(binding, 'routine');
+        func = function() { return 1 + 2; }
+        binding.set(func);
+        expect(binding.routine).toHaveBeenCalledWith(el, func, undefined);
+      });
+
+      it('passes the previously set funcation as the current listener on subsequent calls', function() {
+        spyOn(binding, 'routine');
+        funca = function() { return 1 + 2; };
+        funcb = function() { return 2 + 5; };
+
+        binding.set(funca);
+        expect(binding.routine).toHaveBeenCalledWith(el, funca, undefined);
+
+        binding.set(funcb);
+        expect(binding.routine).toHaveBeenCalledWith(el, funcb, funca);
+      });
+    });
+  });
+});
