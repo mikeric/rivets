5dc8dc74b2b39a229d8848228d1a91c70d0275c6
Zachary Bush <Zachary.bush@meraki.net>
2014-01-28 13:31:52 -0800

    Make the observer test clearer

--- spec/rivets/observer.js
+++ spec/rivets/observer.js
@@ -8,15 +8,18 @@ describe("Rivets.observer", function() {
       }
     };
 
-    var was_called = false;
+    var called_count = 0;
 
     var obs = new rivets._.Observer(rivets, model, 'a.b.c', function() {
-      was_called = true;
+      called_count += 1;
     });
+
+    model.a.b.c = 'bar';
+    expect(called_count).toBe(1);
 
     obs.unobserve();
 
-    model.a.b.c = 'bar';
-    expect(was_called).toBe(false);
+    model.a.b.c = 'baz';
+    expect(called_count).toBe(1);
   });
 });
