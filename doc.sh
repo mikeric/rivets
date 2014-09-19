138c89d5b660aca1d42be1648f97bdd9064f9229
AWinterman <andywinterman@gmail.com>
2014-09-19 14:16:48 -0700

    check for dirty working tree, handle failure to commit

--- doc.sh
+++ doc.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 set -e
+STATUS=$(git status --porcelain 2>/dev/null)
 BRANCH_NAME=$(git symbolic-ref HEAD 2>/dev/null)
 BRANCH_NAME=${BRANCH_NAME##refs/heads/}
+
+if [ -n "$STATUS" ]
+then
+        echo "Dirty working tree; commit your changes then run me again." >&2
+        exit 1
+fi
 
 if [ -z "$MAIN_BRANCH"  ]; then
         MAIN_BRANCH="master"
@@ -9,36 +16,43 @@ fi
 
 if [ "$BRANCH_NAME" != $MAIN_BRANCH ] 
 then
-        echo "Refusing to update documentation from non-master branch:"
-        echo "expect branch $BRANCH_NAME == $MAIN_BRANCH"
-        echo "If you want to view your rendered documentation run: "
-        echo ""
-        echo "    harp server docs "
-        echo ""
+        echo "Refusing to update documentation from non-main branch:" >&2
+        echo "expect branch $BRANCH_NAME == $MAIN_BRANCH" >&2
+        echo "If you want to view your rendered documentation run: " >&2
+        echo "" >&2
+        echo "    harp server docs " >&2
+        echo "" >&2
         exit 1
 fi
 
-# Not sure what to do with this at current, but it should get displayed in the
-# documentation someplace, so I thought I'd put it in the env...
+# The docs should include the version when they build. At current it's just in
+# the commit message.
 export RIVETS_VERSION=$( \
         grep 'version' package.json | \
         awk -F'["]' '{print $4}' \
 )
 
 
-cp -r docs .git
-echo "copying docs to gh-pages"
-git checkout gh-pages
-echo "updating documentation"
+git checkout gh-pages 2>/dev/null 1>/dev/null
+git checkout $MAIN_BRANCH -- docs 2>/dev/null
 rm -rf _harp
 mv docs _harp
-echo "recompiling"
-harp compile _harp docs
+harp compile _harp .
 
-echo "committing changes"
-git add -A
-git commit -a -m "generated documentation for $RIVETS_VERSION"
-echo "When you are ready to publish:"
-echo "     git push origin gh-pages"
-exit 0
+git add -A 2>/dev/null 1>/dev/null
+
+set +e
+git commit -a -m "generated documentation for v$RIVETS_VERSION" 2>/dev/null 1>/dev/null
+
+if [ $? -eq 0 ]
+then
+        echo "documentation updated, publish via:" >&2
+        echo "    git push origin gh-pages:gh-pages" >&2
+else
+        echo "No changes to publish" >&2
+fi
+set -e
+
+git checkout $MAIN_BRANCH 2>/dev/null 1>/dev/null
+
 
