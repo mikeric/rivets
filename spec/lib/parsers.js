47757c5c83291fb030a8576bd8da0ee72b16819f
Ben Styles <enquiries@leeds-ebooks.co.uk>
2015-08-14 14:18:25 +0100

    Quick and dirty getting parseTemplate module into global scope so we can test it

---
+++ spec/lib/parsers.js
@@ -0,0 +1,110 @@
+(function (global, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['exports'], factory);
+  } else if (typeof exports !== 'undefined') {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.parsers = mod.exports;
+  }
+})(this, function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, '__esModule', {
+    value: true
+  });
+
+  // Parser and tokenizer for getting the type and value from a string.
+  exports.parseType = parseType;
+
+  // Template parser and tokenizer for mustache-style text content bindings.
+  // Parses the template and returns a set of tokens, separating static portions
+  // of text from binding declarations.
+  exports.parseTemplate = parseTemplate;
+  var PRIMITIVE = 0;
+  var KEYPATH = 1;
+  var TEXT = 0;
+  var BINDING = 1;
+  function parseType(string) {
+    var type = PRIMITIVE;
+    var value = string;
+
+    if (/^'.*'$|^".*"$/.test(string)) {
+      value = string.slice(1, -1);
+    } else if (string === 'true') {
+      value = true;
+    } else if (string === 'false') {
+      value = false;
+    } else if (string === 'null') {
+      value = null;
+    } else if (string === 'undefined') {
+      value = undefined;
+    } else if (isNaN(Number(string)) === false) {
+      value = Number(string);
+    } else {
+      type = KEYPATH;
+    }
+
+    return { type: type, value: value };
+  }
+
+  function parseTemplate(template, delimiters) {
+    var tokens = [];
+    var length = template.length;
+    var index = 0;
+    var lastIndex = 0;
+
+    while (lastIndex < length) {
+      index = template.indexOf(delimiters[0], lastIndex);
+
+      if (index < 0) {
+        tokens.push({
+          type: TEXT,
+          value: template.slice(lastIndex)
+        });
+
+        break;
+      } else {
+        if (index > 0 && lastIndex < index) {
+          tokens.push({
+            type: TEXT,
+            value: template.slice(lastIndex, index)
+          });
+        }
+
+        lastIndex = index + delimiters[0].length;
+        index = template.indexOf(delimiters[1], lastIndex);
+
+        if (index < 0) {
+          var substring = template.slice(lastIndex - delimiters[1].length);
+          lastToken = tokens[tokens.length - 1];
+
+          if (lastToken && lastToken.type === TEXT) {
+            lastToken.value += substring;
+          } else {
+            tokens.push({
+              type: TEXT,
+              value: substring
+            });
+          }
+
+          break;
+        }
+
+        var value = template.slice(lastIndex, index).trim();
+
+        tokens.push({
+          type: BINDING,
+          value: value
+        });
+
+        lastIndex = index + delimiters[1].length;
+      }
+    }
+
+    return tokens;
+  }
+});
--- spec/rivets/text_template_parser.js
+++ spec/rivets/text_template_parser.js
@@ -1,55 +1,51 @@
-describe("Rivets.TextTemplateParser", function() {
-  var Rivets = rivets._
+describe("parseTemplate()", function() {
+  it("tokenizes a text template", function() {
+    template = "Hello {{ user.name }}, you have {{ user.messages.unread | length }} unread messages."
 
-  describe("parse()", function() {
-    it("tokenizes a text template", function() {
-      template = "Hello {{ user.name }}, you have {{ user.messages.unread | length }} unread messages."
+    expected = [
+      {type: 0, value: "Hello "},
+      {type: 1, value: "user.name"},
+      {type: 0, value: ", you have "},
+      {type: 1, value: "user.messages.unread | length"},
+      {type: 0, value: " unread messages."}
+    ]
 
-      expected = [
-        {type: 0, value: "Hello "},
-        {type: 1, value: "user.name"},
-        {type: 0, value: ", you have "},
-        {type: 1, value: "user.messages.unread | length"},
-        {type: 0, value: " unread messages."}
-      ]
+    results = parsers.parseTemplate(template, ['{{', '}}'])
+    results.length.should.equal(5)
 
-      results = Rivets.TextTemplateParser.parse(template, ['{{', '}}'])
-      results.length.should.equal(5)
+    for (i = 0; i < results.length; i++) {
+      results[i].type.should.equal(expected[i].type)
+      results[i].value.should.equal(expected[i].value)
+    }
+  })
+
+  describe("with no binding fragments", function() {
+    it("should return a single text token", function() {
+      template = "Hello World!"
+      expected = [{type: 0, value: "Hello World!"}]
+
+      results = parsers.parseTemplate(template, ['{{', '}}'])
+      results.length.should.equal(1)
 
       for (i = 0; i < results.length; i++) {
         results[i].type.should.equal(expected[i].type)
         results[i].value.should.equal(expected[i].value)
       }
     })
+  })
 
-    describe("with no binding fragments", function() {
-      it("should return a single text token", function() {
-        template = "Hello World!"
-        expected = [{type: 0, value: "Hello World!"}]
+  describe("with only a binding fragment", function() {
+    it("should return a single binding token", function() {
+      template = "{{ user.name }}"
+      expected = [{type: 1, value: "user.name"}]
 
-        results = Rivets.TextTemplateParser.parse(template, ['{{', '}}'])
-        results.length.should.equal(1)
+      results = parsers.parseTemplate(template, ['{{', '}}'])
+      results.length.should.equal(1)
 
-        for (i = 0; i < results.length; i++) {
-          results[i].type.should.equal(expected[i].type)
-          results[i].value.should.equal(expected[i].value)
-        }
-      })
-    })
-
-    describe("with only a binding fragment", function() {
-      it("should return a single binding token", function() {
-        template = "{{ user.name }}"
-        expected = [{type: 1, value: "user.name"}]
-
-        results = Rivets.TextTemplateParser.parse(template, ['{{', '}}'])
-        results.length.should.equal(1)
-
-        for (i = 0; i < results.length; i++) {
-          results[i].type.should.equal(expected[i].type)
-          results[i].value.should.equal(expected[i].value)
-        }
-      })
+      for (i = 0; i < results.length; i++) {
+        results[i].type.should.equal(expected[i].type)
+        results[i].value.should.equal(expected[i].value)
+      }
     })
   })
 })
