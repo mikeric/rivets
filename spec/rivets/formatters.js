15d8adae9062ef43e1e86f52a6c906cfe09a405d
Jean-Christophe Cazeaux <jccazeaux1979@gmail.com>
2016-02-10 18:27:38 +0100

    Added test on call formatter to check context sent to the function

--- spec/rivets/formatters.js
+++ spec/rivets/formatters.js
@@ -14,5 +14,18 @@ describe("Rivets.formatters", function() {
     it("calls function with arguments", function() {
       rivets.formatters['call'](model.fn, 'foo', 'bar').should.equal('foobar')
     })
+
+    it("calls function with the model as context", function() {
+      model.obj = {
+        name: 'foo',
+        fn: function() {
+          return this.name;
+        }
+      }
+      var el = document.createElement('div')
+      el.setAttribute('rv-text', 'obj.fn | call')
+      rivets.bind(el, model)
+      el.innerText.should.equal('foo')
+    })
   })
 });
